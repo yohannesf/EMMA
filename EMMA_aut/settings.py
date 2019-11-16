2fff0b80c87bb099dc693bbccde868ffa73aa96a
YohannesF <yohannes.fikre@gmail.com>
2019-11-16 12:35:17 +0300

    Added Local Settings import

--- EMMA_aut/settings.py
+++ EMMA_aut/settings.py
@@ -136,3 +136,8 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
